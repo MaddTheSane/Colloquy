758fa77d3068767aa119437aded81a05d1b5c7b1
C.W. Betts <computers57@hotmail.com>
2014-03-10 12:20:28 -0600

    More messing with the Xcode assets.

--- Importer/GetMetadataForFile.h
+++ Importer/GetMetadataForFile.h
@@ -9,7 +9,7 @@
 #ifndef Colloquy__Old__GetMetadataForFile_h
 #define Colloquy__Old__GetMetadataForFile_h
 
-__private_extern__ Boolean GetMetadataForFile( void *thisInterface, CFMutableDictionaryRef attributes, CFStringRef contentTypeUTI, CFStringRef pathToFile );
+__private_extern__ Boolean GetMetadataForFile(void *thisInterface, CFMutableDictionaryRef attributes, CFStringRef contentTypeUTI, CFStringRef pathToFile);
 __private_extern__ Boolean GetMetadataForURL(void* thisInterface, CFMutableDictionaryRef attributes, CFStringRef contentTypeUTI, CFURLRef urlForFile);
 
 
